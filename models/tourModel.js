68b0b5ef6685897c1d96a9ee8a6ee6623a053ffb
Ilia Popov <ilia_popov02@abv.bg>
2023-12-06 08:22:40 +0200

    Add models

---
+++ models/tourModel.js
@@ -0,0 +1,127 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
+      // validate: {
+      //   validator: validator.isAlpha,
+      //   message: 'The name should contain only strings!',
+      // },
+    },
+    slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have groupSize'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Possible difficulties are easy, medium, difficult',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: `Discount price ({VALUE}) must be below regular price`,
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, {
+    lower: true,
+  });
+
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.post(/^find/, function (doc, next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
