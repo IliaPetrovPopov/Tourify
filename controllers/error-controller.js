11cde27a572a8debc889daa715fce7194d6b20d8
Ilia Popov <ilia_popov02@abv.bg>
2023-12-06 08:23:16 +0200

    Add controllers

---
+++ controllers/error-controller.js
@@ -0,0 +1,68 @@
+const AppError = require('../utils/app-error');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data: ${errors.join(' | ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode ?? 500;
+  err.status = err.status ?? 'Error!';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let errorCopy = { ...err };
+
+    if (errorCopy.name === 'CastError') {
+      errorCopy = handleCastErrorDB(errorCopy);
+    }
+
+    if (errorCopy.code === 11000) {
+      errorCopy = handleDuplicateFieldsDB(errorCopy);
+    }
+
+    if (errorCopy.name === 'ValidationError') {
+      errorCopy = handleValidationErrorDB(errorCopy);
+    }
+
+    sendErrorProd(errorCopy, res);
+  }
+};
---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+module.exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'Not yet implemented!',
+  });
+};
+
+module.exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'Not yet implemented!',
+  });
+};
+
+module.exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'Not yet implemented!',
+  });
+};
+
+module.exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'Not yet implemented!',
+  });
+};
+
+module.exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'Not yet implemented!',
+  });
+};
