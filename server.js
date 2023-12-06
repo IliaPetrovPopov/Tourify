1a082369bee127bbde534a4ac4c4e9ef3ffe7b5b
Ilia Popov <ilia_popov02@abv.bg>
2023-12-06 08:20:57 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Exception... Shutting down...');
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+(async () => {
+  await mongoose.connect(DB, {});
+})();
+
+const port = process.env.PORT ?? 3000;
+
+const server = app.listen(port, () => {
+  console.log('Go go go');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection... Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
