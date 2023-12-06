dc7e00b4314c04dc057b33e137be87db00e6a6f8
Ilia Popov <ilia_popov02@abv.bg>
2023-12-06 08:21:55 +0200

    Add utils folder

---
+++ utils/api-features.js
@@ -0,0 +1,56 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    for (let field of excludedFields) {
+      delete queryObj[field];
+    }
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = JSON.parse(
+      queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+    );
+
+    this.query = this.query.find(queryStr);
+
+    return this;
+  }
+
+  sort() {
+    if (Object.keys(this.queryString).length > 0) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt _id');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = +this.queryString.page ?? 1;
+    const limit = +this.queryString.limit ?? 3;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
