b31f3e7743a64e22fb466ab94170f12380d4c6e8
Ilia Popov <ilia_popov02@abv.bg>
2023-12-06 08:20:29 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/app-error');
+const globalErrorHandler = require('./controllers/error-controller');
+
+const tourRouter = require('./routes/tour');
+const userRouter = require('./routes/user');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
