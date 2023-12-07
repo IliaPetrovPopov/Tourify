d881298eb357e44b5090619658a930484c96305e
Ilia Popov <ilia_popov02@abv.bg>
2023-12-07 07:30:49 +0200

    Implement getAllUsers controller function

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,16 @@
-module.exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'Error',
-    message: 'Not yet implemented!',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catch-async');
+
+module.exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      users,
+    },
   });
-};
+});
 
 module.exports.createUser = (req, res) => {
   res.status(500).json({
