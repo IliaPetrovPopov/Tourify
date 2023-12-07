c5c35e4bb2193c26c388a41ddf7385ce177c7998
Ilia Popov <ilia_popov02@abv.bg>
2023-12-07 07:31:45 +0200

    Add routes for log in & sign up

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,12 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require('../controllers/user.JS');
+} = require('../controllers/user.js');
+const { signup, login } = require('../controllers/authentication.js');
 
 const router = express.Router();
+router.post('/signup', signup);
+router.post('/login', login);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
