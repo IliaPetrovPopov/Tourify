f4e49dcb66c2f72853163a6c3468a9a960cd7872
Ilia Popov <ilia_popov02@abv.bg>
2023-12-07 07:30:25 +0200

    Add authentication file Log in & sign up are implemented with jwt

---
+++ controllers/authentication.js
@@ -0,0 +1,52 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catch-async');
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/app-error');
+const bcrypt = require('bcryptjs');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+module.exports.signup = catchAsync(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+});
+
+module.exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  const correct = await user?.correctPassword(password, user?.password);
+  if (!correct) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
