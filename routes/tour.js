b2e7f5c99090c3c90dce4b3a74280a17948b3a12
Ilia Popov <ilia_popov02@abv.bg>
2023-12-08 09:58:24 +0200

    Add protection for getAllTours route

--- routes/tour.js
+++ routes/tour.js
@@ -9,7 +9,7 @@ const {
   getTourStats,
   getMontlyPlan,
 } = require('../controllers/tour');
-
+const { protect } = require('../controllers/authentication');
 const router = express.Router();
 
 // router.param('id', checkId);
@@ -17,7 +17,7 @@ const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
 router.route('/montly-plan/:year').get(getMontlyPlan);
 router.route('/top-5-cheapest-tours').get(aliasTopTours, getAllTours);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id?').get(getTour).patch(modifyTour).delete(deleteTour);
 
 module.exports = router;
