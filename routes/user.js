8830c7eb65f753d55f2a795a59c60f6f7b48a07f
Ilia Popov <ilia_popov02@abv.bg>
2023-12-11 10:35:33 +0200

    Add routes for updating and deleting (deactivating) user data

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,16 @@ const {
   getUser,
   updateUser,
   deleteUser,
+  updateMe,
+  deleteMe,
 } = require('../controllers/user.js');
 const {
   signup,
   login,
   forgotPassword,
   resetPassword,
+  updatePassword,
+  protect,
 } = require('../controllers/authentication.js');
 
 const router = express.Router();
@@ -18,6 +22,9 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
+router.patch('/updatePassword', protect, updatePassword);
+router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
