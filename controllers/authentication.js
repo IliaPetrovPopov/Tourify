edad84d0975f01228a3cbe7762aefc82dc257cd1
Ilia Popov <ilia_popov02@abv.bg>
2023-12-12 12:36:23 +0200

    Add cookies & storing jwt inside

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,6 +8,12 @@ const APIFeatures = require('../utils/api-features');
 const { decode } = require('punycode');
 const { sendEmail } = require('../utils/email');
 
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true,
+};
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -17,6 +23,11 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+  
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -30,7 +41,7 @@ module.exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
+    passwordChangedAt: Date.now(),
     role: req.body.role,
   });
 
