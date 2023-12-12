503dcb8aac9dbd12bba242f75801005ca8d93dd9
Ilia Popov <ilia_popov02@abv.bg>
2023-12-12 12:36:49 +0200

    Add security middlewares against malicious attacks

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// xss-clean is deprecated
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/app-error');
 const globalErrorHandler = require('./controllers/error-controller');
@@ -9,11 +16,43 @@ const userRouter = require('./routes/user');
 
 const app = express();
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 app.use(express.static('./public'));
 
 app.use((req, res, next) => {
