73910456b5303666f8cf500b830303a2ad95af12
Ilia Popov <ilia_popov02@abv.bg>
2023-12-19 14:33:37 +0200

    Add nested routes

--- routes/tour.js
+++ routes/tour.js
@@ -10,14 +10,25 @@ const {
   getMontlyPlan,
 } = require('../controllers/tour');
 const { protect, restrictTo } = require('../controllers/authentication');
+const reviewRouter = require('./review');
 const router = express.Router();
 
 // router.param('id', checkId);
+
+// router
+//   .route('/:tourID/reviews')
+//   .post(protect, restrictTo('user'), createReview);
+
+router.use('/:tourID/reviews', reviewRouter);
 
 router.route('/tour-stats').get(getTourStats);
 router.route('/montly-plan/:year').get(getMontlyPlan);
 router.route('/top-5-cheapest-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id?').get(getTour).patch(modifyTour).delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
+router
+  .route('/:id?')
+  .get(getTour)
+  .patch(modifyTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
