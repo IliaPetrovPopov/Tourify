3d0f76735cdefdc05891405cab62cbb24f9bfbb4
Ilia Popov <ilia_popov02@abv.bg>
2024-01-18 08:05:02 +0200

    Add the login, signup, me, specific tour & me routes in the view router

--- routes/view.js
+++ routes/view.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { getOverview, getTour } = require('../controllers/view');
+const {
+  getOverview,
+  getTour,
+  getLoginForm,
+  getSignupForm,
+  getAccount,
+} = require('../controllers/view');
+const { isLoggedIn, protect } = require('../controllers/authentication');
 
 const router = express.Router();
 
-router.get('/', getOverview);
-
-router.get('/tour', getTour);
+router.get('/', isLoggedIn, getOverview);
+router.get('/tour/:slug', isLoggedIn, getTour);
+router.get('/login', isLoggedIn, getLoginForm);
+router.get('/signup', isLoggedIn, getSignupForm);
+router.get('/me', protect, getAccount);
 
 module.exports = router;
