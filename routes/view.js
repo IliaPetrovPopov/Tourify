c34f0673eb2d96637bc257510eaf65221d85180b
Ilia Popov <ilia_popov02@abv.bg>
2024-01-20 12:59:51 +0200

    Add checkout route & my-tours route

--- routes/view.js
+++ routes/view.js
@@ -5,15 +5,18 @@ const {
   getLoginForm,
   getSignupForm,
   getAccount,
+  getMyTours,
 } = require('../controllers/view');
 const { isLoggedIn, protect } = require('../controllers/authentication');
+const { createBookingCheckout, getBooking } = require('../controllers/booking');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/signup', isLoggedIn, getSignupForm);
 router.get('/me', protect, getAccount);
+router.get('/my-tours', protect, getMyTours);
 
 module.exports = router;
