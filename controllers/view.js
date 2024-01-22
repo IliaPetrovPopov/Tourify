1adf965880bcac4ce9f30debae90a68041dd768e
Ilia Popov <ilia_popov02@abv.bg>
2024-01-22 11:28:49 +0200

    Add controller for view files rendering

--- controllers/view.js
+++ controllers/view.js
@@ -1,4 +1,6 @@
+const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/app-error');
 const catchAsync = require('../utils/catch-async');
 
 module.exports.getOverview = catchAsync(async (req, res, next) => {
@@ -6,12 +8,63 @@ module.exports.getOverview = catchAsync(async (req, res, next) => {
 
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours
+    tours,
   });
 });
 
-module.exports.getTour = (req, res) => {
+module.exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
   res.status(200).render('tour', {
-    title: 'The Bakoville',
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
+
+module.exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Login',
   });
 };
+
+module.exports.getSignupForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Signup',
+  });
+};
+
+module.exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: req.user,
+  });
+};
+
+module.exports.getMyTours = catchAsync(async (req, res, next) => {
+  const bookings = await Booking.find({ user: req.user.id }).populate(
+    'associatedTour'
+  );
+
+  // const tourIDs = bookings.map((booking) => booking.tour);
+  // const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  const tours = bookings.flatMap((booking) => booking.associatedTour);
+
+  // res.status(200).json({
+  //   status: 'success',
+  //   tours,
+  // });
+
+  res.status(200).render('overview', {
+    title: 'My booked tours',
+    tours,
+    user: req.user,
+  });
+});
