b7566e07c878f586c7873ab29fb7a67966bc0d93
Ilia Popov <ilia_popov02@abv.bg>
2024-01-22 12:21:38 +0200

    Add custom alert

---
+++ public/js/alert.js
@@ -0,0 +1,14 @@
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+
+  if (el) el.parentElement.removeChild(el);
+};
+
+export const showAlert = (type, message) => {
+  hideAlert();
+
+  const markup = `<div class="alert alert--${type}">${message}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+  window.setTimeout(hideAlert, 4000);
+};
